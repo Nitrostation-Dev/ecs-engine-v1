a4e81b7095b250a4bf4782d86ead4ce990b14040
Nitrostation-Dev <dev.nitrostation@gmail.com>
2025-08-01 18:54:40 +0530

    Working Component Manager

--- include/component_manager.hpp
+++ include/component_manager.hpp
@@ -4,29 +4,76 @@
 #include "component_array.hpp"
 
 #include <array>
+#include <iostream>
 #include <memory>
 #include <unordered_map>
+
 class ComponentManager {
 private:
 	std::unordered_map<const char*, ComponentId> m_componentIdsMap{};
 	std::unordered_map<ComponentId, std::shared_ptr<IComponentArray>> m_componentArrays{};
 
-	ComponentId m_nextComponentId;
+	ComponentId m_nextComponentId = 0;
 
 public:
 	template <typename T>
 	void RegisterComponent() {
 		const char* typeName = typeid(T).name();
-
+		
+		ComponentId id = m_nextComponentId;
 		m_componentIdsMap.insert({
 			typeName,
-			m_nextComponentId
+			id	
 		});
 		m_componentArrays.insert({
-			m_nextComponentId,
+			id,
 			std::make_unique<ComponentArray<T>>()
 		});
 
+		std::cout << "Registered Component to ECS: " << typeName << " with id: " << id + 0 << std::endl;
+
 		++m_nextComponentId;
+	}
+
+	template<typename T>
+	ComponentId GetComponentId() {
+		const char* typeName = typeid(T).name();
+
+		assert(m_componentIdsMap.find(typeName) != m_componentIdsMap.end() && "Component Not Registered.");
+
+		return m_componentIdsMap[typeName];
+	}
+
+	template<typename T>
+	void AddComponent(Entity entity, T component) {
+		GetComponentArray<T>()->InsertData(entity, component);
+	}
+	
+	template<typename T>
+	void RemoveComponent(Entity entity) {
+		GetComponentArray<T>()->RemoveData(entity);
+	}
+
+	template<typename T>
+	T& GetComponent(Entity entity) {
+		return GetComponentArray<T>()->GetData(entity);
+	}
+
+	template<typename T>
+	void EntityDestroyed(Entity entity) {
+		for (auto const& pair : m_componentArrays) {
+			auto const& component = pair.second;
+			component->EntityDestroyed(entity);
+		}
+	}
+
+private:
+	
+	template <typename T>
+	std::shared_ptr<ComponentArray<T>> GetComponentArray() {
+		const char* typeName = typeid(T).name();
+		assert(m_componentIdsMap.find(typeName) != m_componentIdsMap.end() && "Component Not Registered.");
+
+		return std::static_pointer_cast<ComponentArray<T>>(m_componentArrays[m_componentIdsMap[typeName]]);
 	}
 };
--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,13 @@
 #include <memory>
 
 struct Velocity {
+	const char* name;
+};
+
+struct Transform {
 	float x;
+	float y;
+	float z;
 };
 
 int main() {
@@ -12,4 +18,27 @@ int main() {
 	std::unique_ptr<ComponentManager> cm = std::make_unique<ComponentManager>();
 
 	cm->RegisterComponent<Velocity>();
+	cm->RegisterComponent<Transform>();
+
+	Transform t = {
+		.x = 0.0f,
+		.y = 0.0f,
+		.z = 0.0f,
+	};
+	Velocity v = {
+		.name = 0
+	};
+
+	Entity entity = em->CreateEntity();
+
+	cm->AddComponent(entity, t);
+	cm->AddComponent(entity, v);
+
+	while (true) {
+		Transform& transform = cm->GetComponent<Transform>(entity);
+
+		transform.x += 1.0f;
+
+		printf("%f %f %f\n", transform.x, transform.y, transform.z);
+	}
 }
